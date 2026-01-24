ce6c7229fcde8319e977cc521cf509aafd3351e2
Alvin Dong <alvindong9913@gmail.com>
2026-01-23 22:29:19 -0600

    tweaked ui again

--- mod/visuals/dynatext.lua
+++ mod/visuals/dynatext.lua
@@ -13,5 +13,6 @@ SMODS.DynaTextEffect({
     func = function(dynatext, index, letter)
         local fac = math.pow(math.cos(math.pi * ((G.TIMERS.REAL - dynatext.created_time + 0.3) * 1.8 - index) / 10), 50)
         letter.colour = mix_colours(Multiverse.C.TRANSMUTED_GRADIENT, G.C.UI.TEXT_LIGHT, fac)
+        letter.offset.y = 30 * fac
     end,
 })
