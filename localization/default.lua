1fd7e61c9fa735765369b3cd938dedb6e21204e5
Alvin Dong <alvindong9913@gmail.com>
2026-02-26 11:32:21 -0600

    Update default.lua

--- localization/default.lua
+++ localization/default.lua
@@ -107,7 +107,7 @@ return {
 			du_mul_ench_luck_info = {
 				name = "Enchantment Luck",
 				text = {
-					"Enchantment Luck increases",
+					"{C:mul_enchantedbook}Enchantment Luck{} increases",
 					"the chance that more {C:mul_enchantedbook}Deck",
 					"{C:mul_enchantedbook}Enchantments{} with higher",
 					"levels appear when {C:mul_enchantedbook}Enchanted",
